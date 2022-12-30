3adf39decf87ada87b66f2f3dec7e34c8b17c7ea
affanramadhan23 <affannabilramadhan@gmail.com>
2022-12-30 17:23:35 +0700

    add class BinaryTree

--- BinarySearchTree_071/Program.cs
+++ BinarySearchTree_071/Program.cs
@@ -23,7 +23,32 @@ namespace BinarySearchTree_071
 
     // A Node class consists on three things, the information, reference to the right child, and reference to the left child //
 
-    
+    class BinaryTree
+    {
+        public Node ROOT;
+        public BinaryTree()
+        {
+            ROOT = null; // Initializing ROOT to null //
+        }
+        public void insert(string element) //Insert a node in the binary search tree
+        {
+            Node tmp, parent = null, currentNode = null;
+            Search(element, ref parent, ref currentNode);
+            if (currentNode != null) //Check if the Node to be inserted already inserted or not
+            {
+                Console.WriteLine("Duplicate words not allowed");
+                return;
+            }
+            else    //If the spesified node is not present 
+            {
+                tmp = new Node(element, null, null); //create a Node
+                if (parent == null) //if the tree is empty
+                {
+
+                }
+            }
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
