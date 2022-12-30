b6394812df39bd70ff03271e7bd7334861d220ab
affanramadhan23 <affannabilramadhan@gmail.com>
2022-12-30 18:02:46 +0700

    adds static void main

--- BinarySearchTree_071/Program.cs
+++ BinarySearchTree_071/Program.cs
@@ -61,7 +61,7 @@ namespace BinarySearchTree_071
             //This function searches the currentNode of the spesified Node as well as the current Node its parent//
             currentNode = ROOT;
             parent = null;
-            while ((currentNode !=null) && (currentNode.info != element))
+            while ((currentNode != null) && (currentNode.info != element))
             {
                 parent = currentNode;
                 if (string.Compare(element, currentNode.info) < 0)
@@ -75,7 +75,7 @@ namespace BinarySearchTree_071
             if (ROOT == null)
             {
                 Console.WriteLine("tree is empty");
-                return ;
+                return;
             }
             if (ptr == null)
             {
@@ -113,11 +113,55 @@ namespace BinarySearchTree_071
                 Console.WriteLine(ptr.info + " ");
             }
         }
-    }
-    internal class Program
-    {
+
         static void Main(string[] args)
         {
+            BinaryTree x = new BinaryTree();
+            while (true)
+            {
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("1. Implement insert operation");
+                Console.WriteLine("2. Perform inorder traversal");
+                Console.WriteLine("3. Perform preorder traversal");
+                Console.WriteLine("4. Perfrom postorder traversal");
+                Console.WriteLine("5. Exit");
+                Console.Write("\nEnter your choice (1-5) :");
+                char ch = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine();
+                switch (ch)
+                {
+                    case '1':
+                        {
+                            Console.Write("Enter a word: ");
+                            String word = Console.ReadLine();
+                            x.insert(word);
+                        }
+                        break;
+                    case '2':
+                        {
+                            x.inorder(x.ROOT);
+                        }
+                        break;
+                    case '3':
+                        {
+                            x.preorder(x.ROOT);
+                        }
+                        break;
+                    case '4':
+                        {
+                            x.postorder(x.ROOT);
+                        }
+                        break;
+                    case '5':
+                        return;
+                    default:
+                        {
+                            Console.WriteLine("Invalid Option");
+                            break;
+                        }
+                }
+            }
         }
     }
 }
+
